be508f1929bb3463c2ea7314ef40e76c4a7b9813
daranzolin <daranzolin@gmail.com>
2018-05-09 20:14:16 -0700

    day 3

--- daily.js
+++ daily.js
@@ -26,3 +26,53 @@ function calculateAge(yob) {
   
   let years = [1987, 1988, 2000];
   console.log(calcAnything(years, calculateAge));
+
+//Day 3 Call, Apply, Bind
+
+// The call() method calls a function with a given this value and arguments provided individually.
+// While the syntax of this function is almost identical to that of apply(), 
+// the fundamental difference is that call() accepts an argument list, 
+// while apply() accepts a single array of arguments.
+
+// Call/apply call the function immediately, whereas bind returns a function that, when later executed, 
+// will have the correct context set for calling the original function. 
+
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  
+  function Food(name, price) {
+    Product.call(this, name, price);
+    this.category = 'food';
+  }
+  
+console.log(new Food('cheese', 5).name);
+  // expected output: "cheese"
+
+var numbers = [5, 6, 2, 3, 7];
+
+var max = Math.max.apply(null, numbers);
+
+console.log(max);
+// expected output: 7
+
+var min = Math.min.apply(null, numbers);
+
+console.log(min);
+// expected output: 2
+
+var module = {
+    x: 42,
+    getX: function() {
+      return this.x;
+    }
+  }
+  
+  var unboundGetX = module.getX;
+  console.log(unboundGetX()); // The function gets invoked at the global scope
+  // expected output: undefined
+  
+  var boundGetX = unboundGetX.bind(module);
+  console.log(boundGetX());
+  // expected output: 42
